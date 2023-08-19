9c1429b2e6208b086e50027d91a78374e507506e
mehdi-harakat <mehdiharakat20@gmail.com>
2023-08-19 03:46:04 +0100

    create qualification

--- Javascript/main.js
+++ Javascript/main.js
@@ -1,9 +1,9 @@
 const cursorMove = document.querySelector(".cursor-anime");
-const upBtn = document.querySelector('.up-btn');
+const upBtn = document.querySelector(".up-btn");
 const headerOne = document.querySelector("header");
 const navBar = document.querySelector(".nav-bar");
-const servCards = document.querySelectorAll('.services .cards .box');
-const servHolder = document.querySelectorAll('.services .cards .holder');
+const servCards = document.querySelectorAll(".services .cards .box");
+const servHolder = document.querySelectorAll(".services .cards .holder");
 
 function closeNav() {
 	navBar.classList.toggle("active");
@@ -11,50 +11,56 @@ function closeNav() {
 
 window.addEventListener("scroll", () => {
 	if (scrollY >= 90) {
-		headerOne.style.boxShadow =
-			"rgba(149, 157, 165, 0.2) 0px 8px 24px";
-    } else {
-		headerOne.style.boxShadow =
-			"none";
-  }
+		headerOne.style.boxShadow = "rgba(149, 157, 165, 0.2) 0px 8px 24px";
+	} else {
+		headerOne.style.boxShadow = "none";
+	}
 
-  if (scrollY >= 520 ) {
-    upBtn.classList.add('active');
-  } else {
-    upBtn.classList.remove('active');
-  }
+	if (scrollY >= 520) {
+		upBtn.classList.add("active");
+	} else {
+		upBtn.classList.remove("active");
+	}
 });
 
-
 function upClick() {
-  window.scrollTo(0, 0);
+	window.scrollTo(0, 0);
 }
 
-
 window.addEventListener("mousemove", (e) => {
-  let x = e.clientX;
-  let y = e.clientY;
-  cursorMove.style.left = `${x}px`;
-  cursorMove.style.top = `${y}px`;
+	let x = e.clientX;
+	let y = e.clientY;
+	cursorMove.style.left = `${x}px`;
+	cursorMove.style.top = `${y}px`;
 
-  if (e.target.classList.contains("touch")) {
-    cursorMove.classList.add("active");
-  } else {
-    cursorMove.classList.remove("active");
-  }
-})
-
+	if (e.target.classList.contains("touch")) {
+		cursorMove.classList.add("active");
+	} else {
+		cursorMove.classList.remove("active");
+	}
+});
 
 function closePopUp(e) {
-  e.parentElement.parentElement.classList.remove("active");
+	e.parentElement.parentElement.classList.remove("active");
 }
 
 servCards.forEach((e) => {
-  e.addEventListener('click', () => {
-    servHolder.forEach((el) => {
-      if (e.dataset.count === el.dataset.count) {
-        el.classList.add("active");
-      }
-    })
-  })
-})
+	e.addEventListener("click", () => {
+		servHolder.forEach((el) => {
+			if (e.dataset.count === el.dataset.count) {
+				el.classList.add("active");
+			}
+		});
+	});
+});
+
+function educExper(e) {
+	let postEduction = document.querySelectorAll(".qualification .infos .post");
+	for (post of postEduction) {
+		if (e.dataset.catching === post.dataset.catching) {
+      post.classList.add('active')
+    } else {
+      post.classList.remove('active')
+    }
+	}
+}
